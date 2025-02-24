1e2f6f31c38a1dd412455935697d0b83b8de8426
Popsapple <hyeongr@visang.com>
2025-02-24 16:23:20 +0900

    frist setting

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ReactDOM.render(
+	<React.StrictMode>
+  		<div>Hello</div>
+  	</React.StrictMode>,
+  	document.getElementById('app-root')
+);
