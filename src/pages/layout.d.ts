1b8578fde5776051c5949db44b6ce269ac37aeb0
popsapple <popsapple@users.noreply.github.com>
2025-02-28 08:20:25 +0000

    deploy: f99786e59c44459608dc33b2160524e8b01b4882

--- src/pages/layout.d.ts
+++ src/pages/layout.d.ts
@@ -1,4 +1,4 @@
-import '@styles/layout.scss';
+import '@styles/layout.css';
 declare const Layout: (props: any) => any;
 export default Layout;
 //# sourceMappingURL=../../../src/pages/layout.d.ts.map
