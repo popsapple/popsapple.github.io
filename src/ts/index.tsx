3fe647cafa7795fff28e71c127b9ba465669bbe7
Popsapple <hyeongr@visang.com>
2025-03-03 00:10:41 +0900

    deploy

--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import Layout from '@pages/layout';
-// import ReactDOM from 'react-dom';
-// ReactDOM.render(
-// 	<React.StrictMode>
-//   		<div>Hello</div>
-//   	</React.StrictMode>,
-//   	document.getElementById('app-root')
-// );
-
+const now = Date.now();
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('app-root');
-console.log(container);
+console.log('DATETIME:', now);
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(<React.StrictMode>
 	<Layout />
