85b0a03180ce0f3b172e0860abedc3e5ea240320
popsapple <popsapple@users.noreply.github.com>
2025-03-02 15:07:43 +0000

    deploy: 7fde531722f5c0e0da93aa677bb89ef31f823104

---
+++ src/utils/Cursor.d.ts
@@ -0,0 +1,11 @@
+declare class Cursor {
+    x: number;
+    y: number;
+    r: number;
+    element: HTMLElement;
+    constructor(x: number, y: number, r: number);
+    move(x: number, y: number): void;
+    click(): void;
+}
+export default Cursor;
+//# sourceMappingURL=../../../src/utils/Cursor.d.ts.map
