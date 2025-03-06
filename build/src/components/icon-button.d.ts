cd4bd5dd73f2336e78537c1446ec3abd252f636f
Popsapple <hyeongr@visang.com>
2025-03-06 17:36:31 +0900

    deploy

--- build/src/components/icon-button.d.ts
+++ build/src/components/icon-button.d.ts
@@ -1,4 +1,4 @@
-import React from 'react';
+import { JSX } from 'react';
 declare const Items: {
     DOT1: string;
     REACT: string;
@@ -13,6 +13,11 @@ declare const Items: {
     SVELTE: string;
     DOT2: string;
 };
-declare const IconButton: (props: any) => React.JSX.Element;
+export type ItemKeys = keyof typeof Items;
+declare const IconButton: (props: {
+    type: ItemKeys;
+    children?: JSX.Element;
+    onClick: (type: ItemKeys) => void;
+}) => JSX.Element;
 export { IconButton, Items };
 //# sourceMappingURL=../../../src/components/icon-button.d.ts.map
