fd4366a53cb9efdc6cc99777de8223a338173cb0
Popsapple <hyeongr@visang.com>
2025-03-09 00:14:42 +0900

    deploy

---
+++ build/src/ts/expriences.d.ts
@@ -0,0 +1,13 @@
+export declare const Histories: {
+    company: string;
+    date: string;
+    explain: string;
+}[];
+export declare const Projects: {
+    title: string;
+    date: string;
+    skills: string;
+    account: number;
+    explain: string;
+}[];
+//# sourceMappingURL=../../../src/ts/expriences.d.ts.map
